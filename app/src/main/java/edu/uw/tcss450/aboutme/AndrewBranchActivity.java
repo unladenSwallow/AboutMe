e7771a6b426752650ef1c9814b83ab62617d7def
dionmerz <dionmerz@gmail.com>
2016-10-11 18:26:48 -0700

    Andrew Branch Activity added.

---
+++ app/src/main/java/edu/uw/tcss450/aboutme/AndrewBranchActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.tcss450.aboutme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndrewBranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_andrew_branch);
+    }
+}
